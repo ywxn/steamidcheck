a2642feb303784659910aef5f6507d5b4a179093
ywxn <91235043+ywxn@users.noreply.github.com>
2022-12-19 19:59:49 -0800

    Update steamidchecker.py

--- steamidchecker.py
+++ steamidchecker.py
@@ -1,27 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
 
-    
-def parseWords():
-    with open('words_alpha.txt') as list_words:
-        words = set(list_words.read().split())
-        return words
-
 def check(id):
-    if BeautifulSoup(requests.get("https://steamcommunity.com/id/" + id).content, 'html.parser').select_one("#message > h3:nth-child(3)") is not None:
+    if len(id) == 1:
+        return False
+    elif BeautifulSoup(requests.get("https://steamcommunity.com/id/" + id).content, 'html.parser').select_one("#message > h3:nth-child(3)") is not None:
         return True
     else:
         return False
 
 def main():
-    f = open("freeids.txt", "a")
-    words = parseWords()
+    f = open('freeids.txt', 'a')
+    words = open('words_alpha.txt').read().splitlines()
+    total = len(words)
+    current = 0
     for i in words:
-        print("Checking " + i + "...")
+        current = current + 1
+        print('Checking ' + i + '...' + ' | ' + str(current) + '/' + str(total))
         if check(i) == True:
-            print(i + " is free!")
-            f.write(i+"\n")
+            print(i + ' is free!')
+            f.write(i+'\n')
             f.flush()
+    print('FINISHED!')
 
 
 if __name__ == "__main__":
